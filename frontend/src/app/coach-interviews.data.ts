3d37fb3e4c68bcfdc2f30e01cf30945b210c4467
Atanas Ladzhov <ladjo@gbg.bg>
2026-04-29 12:22:54 +0300

    Update coach-interviews.data.ts

--- frontend/src/app/coach-interviews.data.ts
+++ frontend/src/app/coach-interviews.data.ts
@@ -41,6 +41,43 @@ export const coachInterviews: Record<string, CoachInterview> = {
         answer: "I think the best personal moment I had was when I won the best painted team in Eurobowl (Malta). Next in line are all the award ceremonies we have in our local tournaments - that's always my favorite part of our hobby - having happy people enjoying what they're doing. As for results - I remember vividly the first Euro we attended back in Wales (2018) and the World Cup in Spain (2023). I've managed to finish 13th in Wales (5-1) and 63rd in Spain (6-2-1). In Spain our team played very well and we finished in the middle of all teams (160th out of 372 - with 2232 players total in the tournament). The other memorable tournament was the first World Cup in Austria when I've managed to not score the winning touchdown in two games by rolling snake eyes for the rush in the last turn."
       }
     ]
+  },
+  'Nikolay Arabadzhiev': {
+    intro: `Our Blood Bowl leagues admin`,
+    questions: [
+      {
+        question: "When and how did you start playing?",
+        answer: "I started with Blood Bowl Chaos edition on PC around 2014. I couldn't make much sense of it but always enjoyed turn based strategy games like X-Com and it looked fun. I mostly watched cKnoor's tutorials on youtube until the BB2016 was announced."
+      },
+      {
+        question: "Who introduced you to Blood Bowl",
+        answer: "I found the game by myself looking through Steam discount deals. Initially I overlooked Blood Bowl as it seemed like a NFL/FIFA game, but my interest peaked when I realized it's turn based."
+      },
+      {
+        question: "1-5 - how competitive are you",
+        answer: "For BB I would say 2. It's hard to try-hard in a dice game, but it is fun to consider probabilities and try to play to the best of my abilities."
+      },
+      {
+        question: "Which is your favorite team",
+        answer: "While I haven't yet played all the teams, I find Imperial Nobility to fit me the best so far. I am excited to give OWA a try with the new ruleset."
+      },
+      {
+        question: "Who is your favorite player",
+        answer: "I don't think I have one that really stands out, I look at the game in more of a team vibe along with the model sculpts. I guess to give an answer, the human linemen as the baseline of average, universally ok, but not special. I look at the models as very representative of the concept of BB."
+      },
+      {
+        question: "If there is one thing you can change in Blood Bowl, what would it be",
+        answer: "Minor changes to streamline rules and rules writing. I don't like rules about something that only works on the first Tuesday of the month before noon in the summer..."
+      },
+      {
+        question: "What are some of your most memorable moments",
+        answer: "Uphilling a troll into a KO and triple skulling a block ogre into a knoblar (the dumb fun ones). The first Eurobowl (Europen) experience in Wales 2018 was special, a very fun time and meeting so many people who love the same hobby. Also the first time we played in the actual Eurobowl as a national team in Greece in 2024. Everyone seemed to feel more relaxed and we got to have drinks with the Germany team which was fun."
+      },
+      {
+        question: "What do you like about Blood Bowl in general",
+        answer: "It is a non-war related table top game which is mostly miniature-agnostic. It gives me creative freedom for the hobby side and it's probably the most comfortable duration to banter and chat over a drink. Being a dice game, I get to witness emergent stories unfold and it's hard to take it too seriously which makes it a chill social activity."
+      }
+    ]
   }
 };
 

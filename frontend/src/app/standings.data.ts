3482969f5dcf1a25bb4b528a1010cc39550962b4
Atanas Ladzhov <ladjo@gbg.bg>
2026-06-02 07:55:02 +0300

    Update standings.data.ts

--- frontend/src/app/standings.data.ts
+++ frontend/src/app/standings.data.ts
@@ -1,20 +1,19 @@
 import { RankingEntry } from './app-data.model';
 
 export const standingsEntries: RankingEntry[] = [
-  { country: 'bg', coach: 'rorarimbo', points: 11, touchdowns: 6, casualties: 8 },
-  { country: 'bg', coach: 'Martin Pearson', points: 9, touchdowns: 3, casualties: 9 },
-  { country: 'bg', coach: 'Vasil Baltadzhiev', points: 8, touchdowns: 3, casualties: 5 },
-  { country: 'bg', coach: 'Ubik Spray Can', points: 12, touchdowns: 8, casualties: 16 },
-  { country: 'bg', coach: 'Emil Tzanev', points: 9, touchdowns: 7, casualties: 11 },
-  { country: 'bg', coach: 'Edward Chawner', points: 9, touchdowns: 4, casualties: 12 },
-  { country: 'bg', coach: 'mozz', points: 12, touchdowns: 6, casualties: 17 },
-  { country: 'bg', coach: 'Nikolay Arabadzhiev', points: 5, touchdowns: 5, casualties: 14 },
+  { country: 'bg', coach: 'rorarimbo', points: 19, touchdowns: 9, casualties: 13 },
+  { country: 'bg', coach: 'Martin Pearson', points: 20, touchdowns: 12, casualties: 12 },
+  { country: 'bg', coach: 'Vasil Baltadzhiev', points: 17, touchdowns: 6, casualties: 12 },
+  { country: 'bg', coach: 'Ubik Spray Can', points: 17, touchdowns: 10, casualties: 25 },
+  { country: 'bg', coach: 'Emil Tzanev', points: 13, touchdowns: 10, casualties: 13 },
+  { country: 'bg', coach: 'Edward Chawner', points: 11, touchdowns: 6, casualties: 17 },
+  { country: 'bg', coach: 'Atanas Ladzhov', points: 19, touchdowns: 8, casualties: 25 },
+  { country: 'bg', coach: 'Nikolay Arabadzhiev', points: 8, touchdowns: 7, casualties: 18 },
   { country: 'bg', coach: 'Petar Rimenov', points: 2, touchdowns: 2, casualties: 7 },
-  { country: 'bg', coach: 'Biromryn', points: 1, touchdowns: 3, casualties: 5 },
+  { country: 'bg', coach: 'Biromryn', points: 7, touchdowns: 8, casualties: 6 },
   { country: 'ro', coach: 'eFTy', points: 11, touchdowns: 10, casualties: 7 },
   { country: 'ro', coach: 'Test Prime', points: 9, touchdowns: 4, casualties: 5 },
   { country: 'ro', coach: 'Tiberu Stoica', points: 6, touchdowns: 4, casualties: 12 },
   { country: 'bg', coach: 'SGJack', points: 7, touchdowns: 4, casualties: 10 },
-  { country: 'bg', coach: 'Огнян Дойчинов', points: 1, touchdowns: 2, casualties: 3 },
+  { country: 'bg', coach: 'Огнян Дойчинов', points: 2, touchdowns: 5, casualties: 11 },
 ];
-
